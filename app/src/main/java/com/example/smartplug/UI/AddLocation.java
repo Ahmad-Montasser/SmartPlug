c0b954a7a1231a5c8eb96ff3de940c907d88f8bc
Ahmad-Montasser <ahmad.montasser@yahoo.com>
2020-12-21 21:47:24 +0200

    App Navigation Drawer Added

---
+++ app/src/main/java/com/example/smartplug/UI/AddLocation.java
@@ -0,0 +1,15 @@
+package com.example.smartplug.UI;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import com.example.smartplug.R;
+
+public class AddLocation extends Fragment {
+    public AddLocation() {
+        super(R.layout.add_location_layout);
+    }
+
+}
---
+++ app/src/main/java/com/example/smartplug/UI/Plugs.java
@@ -0,0 +1,13 @@
+package com.example.smartplug.UI;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import com.example.smartplug.R;
+
+public class Plugs extends Fragment {
+public Plugs(){
+    super(R.layout.activity_main);
+
+}
+}
